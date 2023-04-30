f68c55effe1b7ae48b7649e08847097edd973768
King-Z-101 <111461124+King-Z-101@users.noreply.github.com>
2023-04-30 12:50:53 -0700

    new linkedList

--- LinkedList.java
+++ LinkedList.java
@@ -1,3 +1,8 @@
-public class LinkedList {
-
+public class Linked_List {
+    private Node head;
+    private int num_items;
+    public Linked_List() {
+        this.head = new Node(1,2,1);
+        this.num_items = 0;
+    }
 }
