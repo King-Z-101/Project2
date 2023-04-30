22e3fbe4f8159876ba49ff6d7c86c7604e82cc46
King-Z-101 <111461124+King-Z-101@users.noreply.github.com>
2023-04-30 13:09:29 -0700

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,8 +1,8 @@
 public class Linked_List {
-    private Node head;
+    public Node head;
     private int num_items;
     public Linked_List() {
-        this.head = new Node(1,2,1);
+        this.head = new Node(0); //default head pointer
         this.num_items = 0;
     }
 }
