b2da62e71e8d4730c2ab46d8f187550e6a3b553a
King-Z-101 <111461124+King-Z-101@users.noreply.github.com>
2023-04-30 13:08:32 -0700

    Update FileProcessor.java

--- FileProcessor.java
+++ FileProcessor.java
@@ -10,11 +10,16 @@ public class FileProcessor {
      * @param filePath Path to a file containing arithmetic expressions.
      */
     public static void processFile(String filePath) {
+        Linked_List numbers = new Linked_List();
         File infile = new File(filePath);
         try (Scanner scan = new Scanner(infile)) {
             while (scan.hasNext()) {
                 // TODO: Process each line of the input file here.
                 String line = scan.nextLine();
+                for (int i = 0; i < line.length(); i++){
+                    Node number = new Node(line.charAt(i));
+                    numbers.head = number;
+                }
                 System.out.println(line);
             }
         } catch (FileNotFoundException e) {
