d9b24cebec7c7a75074d54a0543f3ccf7c4b2468
King-Z-101 <111461124+King-Z-101@users.noreply.github.com>
2023-05-03 20:25:36 -0700

    Took out the prev

--- Node.java
+++ Node.java
@@ -1,7 +1,7 @@
 public class Node {
     private int item;
-    private Node next = this;
-    private Node prev = this;
+    private Node next;
+    //private Node prev = this;
     //constructor
     public Node(int item) {
         this.item = item;
@@ -10,16 +10,20 @@ public class Node {
     public Node getNext(){
         return this.next;
     }
-    public Node getPrev(){
-        return this.prev;
-    }
+//    public Node getPrev(){
+//        return this.prev;
+//    }
 
 
     // setters
     public void setNext(Node node){
         this.next = node;
     }
-    public void setPrev(Node node){
-        this.prev = node;
+
+    public int getItem() {
+        return item;
     }
+//    public void setPrev(Node node){
+//        this.prev = node;
+//    }
 }
