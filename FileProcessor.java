446d1c52c1477ad8ce52e136a35e53c63820f568
King-Z-101 <111461124+King-Z-101@users.noreply.github.com>
2023-05-05 16:42:42 -0700

    Should work for add B)

--- FileProcessor.java
+++ FileProcessor.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class FileProcessor {
@@ -10,47 +11,67 @@ public class FileProcessor {
      * @param filePath Path to a file containing arithmetic expressions.
      */
     public static void processFile(String filePath) {
-        Linked_List numbers = new Linked_List();
+        //allowed ascii value
+        //int[] deez = {48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 43, 42, 94};
+        //LinkedList numbers = new LinkedList();
         File infile = new File(filePath);
         try (Scanner scan = new Scanner(infile)) {
             while (scan.hasNext()) {
                 // TODO: Process each line of the input file here.
                 String line = scan.nextLine();
+                LinkedList left = new LinkedList();
+                LinkedList right = new LinkedList();
+                boolean leftSide = true;
+                boolean rightSide = false;
+
+                int operator = 0;
+//                if (n != 48 && n != 49 && n != 50 && n != 51 && n != 52 && n != 53 && n != 54 && n != 55 && n != 56
+//                        && n != 57 && n != 58 && n != 43 && n != 42 && n != 94) {
+//                    continue;
                 for (int i = 0; i < line.length(); i++){
-                    char c = line.charAt(i);   //u can use getNumericValue to find value/number passed
-                    Node number = new Node(c);
-                    //numbers.head = number;
-                    numbers.num_items++;
-                    if (numbers.num_items == 1){
-                        numbers.head = number;
-                        continue;
+                    int n = line.charAt(i);//u can use getNumericValue to find value/number passed
+                    if (n == 32){continue;}
+                    if (leftSide == true){
+                        if (n == 43){
+                            leftSide = false;
+                            rightSide = true;
+                            operator = 1;
+                            continue;
+
+                        }
+                        else if (n == 42){
+                            leftSide = false;
+                            rightSide = true;
+                            operator = 2;
+                            continue;
+                        }
+                        else if (n == 94){
+                            leftSide = false;
+                            rightSide = true;
+                            operator = 3;
+                            continue;
+                        }
+                        else{
+                            left.add_to_list(Character.getNumericValue(n));
+                        }
                     }
-                    else{
-                        number.setNext(numbers.head);
-                        numbers.head = number;
+                    if (rightSide == true){
+                        right.add_to_list(Character.getNumericValue(n));
                     }
                 }
                 System.out.println(line);
-                Node indexNode = numbers.head;
-                for (int i = 0; i < numbers.num_items; i++){
-                    //start at header node and move through the linked list until we find the operator we're working with
-                    if (indexNode.getItem() == 43) { //addition ascii
-                        Addition add = new Addition();
-                        add.add(numbers);
-
-                    }
-                    else if (indexNode.getItem() == 42){ //Multiplication ascii
-                        Multiplication mult = new Multiplication();
-                        mult.multiplication(numbers);
-
-                    }
-                    else if (indexNode.getItem() == 94){   //exponent ascii
-                        Exponential exponent = new Exponential();
-                        exponent.exponent(numbers);
-
-                    }
-                    indexNode = indexNode.getNext();
+                if (operator == 1){
+                    Addition addition = new Addition();
+                    addition.add(left,right);
                 }
+//                else if (operator == 2){
+//                    Multiplication multiplication = new Multiplication();
+//                    Multiplication.mult(left,right);
+//                }
+//                else if (operator == 3){
+//                    Exponential exponential = new Exponential();
+//                    exponential.exponent(left, right);
+//                }
             }
         } catch (FileNotFoundException e) {
             System.out.println("File not found: " + infile.getPath());
