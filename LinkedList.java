5e0ad27f34f9a20338fd50079ba91bd75c903240
King-Z-101 <111461124+King-Z-101@users.noreply.github.com>
2023-05-06 12:26:09 -0700

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,23 +1,24 @@
 public class LinkedList {
     public Node head;
     public int num_items;
-
     public LinkedList() {
         this.head = null; //default head pointer
         this.num_items = 0;
     }
+
     // add_to_list will be used to build the linked list based on the file input
-    public void add_to_list(int number){
+    public void add_to_list(int number) {
         Node newNode = new Node(number);
-        if (head == null){
+        this.num_items++;
+        if (head == null) {
             head = newNode;
 
-        }
-        else {
+        } else {
             Node current = head;
-            while (current.getNext() != null){
+            while (current.getNext() != null) {
                 current = current.getNext();
             }
             current.setNext(newNode);
         }
     }
+}
