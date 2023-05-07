2f54714a77e34ff6a303061c22742f8d27fa2498
King-Z-101 <111461124+King-Z-101@users.noreply.github.com>
2023-05-07 13:56:29 -0700

    Using algorithm in project doc

--- Exponentiation.java
+++ Exponentiation.java
@@ -1,5 +1,52 @@
 public class Exponential {
-    public int exponent(Linked_List numbers){
-        return 0;
+    public String exponent(LinkedList list1, LinkedList list2){
+        int result;
+        StringBuilder x = new StringBuilder();
+        StringBuilder n = new StringBuilder();
+        boolean left = true;
+        boolean right = false;
+        boolean loopContinue = true;
+        Node leftInt = list1.head;
+        Node rightInt = list2.head;
+        while (loopContinue == true) {
+            while (left == true) {
+                x.append(leftInt.getItem());
+                if (leftInt.getNext() == null) {
+                    left = false;
+                    right = true;
+                    continue;
+                } else {
+                    leftInt = leftInt.getNext();
+                }
+            }
+
+            while (right == true) {
+                n.append(rightInt.getItem());
+                if (rightInt.getNext() == null) {
+                    right = false;
+                    loopContinue = false;
+                    continue;
+                } else {
+                    rightInt = rightInt.getNext();
+                }
+            }
+        }
+        //now we have the x^n as strings (need to be converted to integers
+        int integer = Integer.parseInt(x.toString());
+        int exponent = Integer.parseInt(n.toString());
+        //use the algorithm to find the result (call exp_by_squaring)
+        result = exp_by_squaring(integer, exponent);
+        return String.valueOf(result);
+    }
+    public int exp_by_squaring(int x, int n) {
+        if (n < 0) {
+            return exp_by_squaring(1 / x, -n);
+        }
+        else if (n == 0) {return 1;}
+        //check if number is even
+        else if (n % 2 == 0){return exp_by_squaring(x * x, n / 2);}
+        //check if number is odd
+        else if (n % 2 != 0) {return x * exp_by_squaring(x * x, (n - 1) / 2);}
+        return x;
     }
 }
