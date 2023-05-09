58a7cce3a161a3066218978ba0762e2a7ad52d5f
King-Z-101 <111461124+King-Z-101@users.noreply.github.com>
2023-05-09 13:52:40 -0700

    Update FileProcessor.java

--- FileProcessor.java
+++ FileProcessor.java
@@ -1,6 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.HashSet;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class FileProcessor {
@@ -11,11 +11,10 @@ public class FileProcessor {
      * @param filePath Path to a file containing arithmetic expressions.
      */
     public static void processFile(String filePath) {
-        //allowed ascii value
-        //int[] deez = {48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 43, 42, 94};
-        //LinkedList numbers = new LinkedList();
         File infile = new File(filePath);
-        try (Scanner scan = new Scanner(infile)) {
+        //PrintWriter outFile = new PrintWriter("output.txt");
+        try (Scanner scan = new Scanner(infile);
+             PrintWriter outFile = new PrintWriter("output.txt")){
             while (scan.hasNext()) {
                 // TODO: Process each line of the input file here.
                 String line = scan.nextLine();
@@ -25,9 +24,7 @@ public class FileProcessor {
                 boolean rightSide = false;
 
                 int operator = 0;
-//                if (n != 48 && n != 49 && n != 50 && n != 51 && n != 52 && n != 53 && n != 54 && n != 55 && n != 56
-//                        && n != 57 && n != 58 && n != 43 && n != 42 && n != 94) {
-//                    continue;
+
                 for (int i = 0; i < line.length(); i++){
                     int n = line.charAt(i);//u can use getNumericValue to find value/number passed
                     if (n == 32){continue;}
@@ -62,23 +59,29 @@ public class FileProcessor {
                 System.out.println(line);
                 String outputLine = "";
                 Format format = new Format();
+                boolean plugText = false;
                 String result = "";
                 if (operator == 1){
                     Addition addition = new Addition();
                     result = addition.add(left,right);
                     //correctly formats the output line (new class created)
                     outputLine = format.formatOutput(left, right, operator, result);
+                    plugText = true;
                 }
                 else if (operator == 2){
                     Multiplication Mult = new Multiplication();
                     result = Mult.multiplication(left,right);
                     outputLine = format.formatOutput(left, right, operator, result);
+                    plugText = true;
                 }
                 else if (operator == 3){
                     Exponential exponential = new Exponential();
                     result = exponential.exponent(left, right);
                     outputLine = format.formatOutput(left, right, operator, result);
-                    return;
+                    plugText = true;
+                }
+                if (plugText == true){
+                    outFile.println(outputLine);
                 }
             }
         } catch (FileNotFoundException e) {
