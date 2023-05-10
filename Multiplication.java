0ad3233286318a2f998ae2b3ea5de0a6aaca2a46
michaelalbertslo <114457545+michaelalbertslo@users.noreply.github.com>
2023-05-09 20:21:02 -0700

    Update Multiplication.java
    Working rn but needs more test cases

--- Multiplication.java
+++ Multiplication.java
@@ -2,64 +2,75 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Multiplication {
-    public String multiplication(LinkedList list1, LinkedList list2){
+    public String multiplication(LinkedList list1, LinkedList list2) {
         Addition add = new Addition();
-        // list 2 will be the "bottom" numbers of partial products multiplication
-        if (list1.head == null || list2.head == null){
+        if (list1.head == null || list2.head == null) {
             return "";
         }
         Node current2 = list2.head;
         LinkedList products1 = new LinkedList();
         LinkedList products2 = new LinkedList();
         int iterations = 0;
-        String product1_string = "";
-        while (!(current2 == null)){
+        while (!(current2 == null)) {
             LinkedList partialProduct = new LinkedList();
             int carry = 0;
             Node current1 = list1.head;
-            // nested loop starts
-            while(!(current1 == null)) {
-                // multiply products here and create a string of each
+            while (!(current1 == null)) {
                 int product = (current1.getItem() * current2.getItem()) + carry;
                 int digit = product % 10;
                 carry = product / 10;
-                partialProduct.add_to_list(digit);
+                partialProduct.add_to_tail(digit);
                 current1 = current1.getNext();
             }
             if (carry > 0) {
-                // carry could be multiple digit, so split carry into individual digits.
                 List<Integer> carry_list = new ArrayList<>();
-                carry_list.add(carry);
                 while (carry > 0) {
                     carry_list.add(carry % 10);
                     carry /= 10;
                 }
                 for (int i = carry_list.size() - 1; i >= 0; i--) {
-                    partialProduct.add_to_list(carry_list.get(i));
+                    partialProduct.add_to_tail(carry_list.get(i));
                 }
             }
-            iterations ++;
-            if (iterations == 1){
+            iterations++;
+            if (iterations == 1) {
                 products1 = partialProduct;
-           } else {
+            } else {
+                int numZeros = iterations - 1;
+                LinkedList shifted = new LinkedList();
+                Node shiftedNode = products1.head;
+                while (shiftedNode != null) {
+                    shifted.add_to_tail(shiftedNode.getItem());
+                    shiftedNode = shiftedNode.getNext();
+                }
+                for (int i = 0; i < numZeros; i++) {
+                    shifted.addZeroToEnd();
+                }
                 products2 = partialProduct;
+
+                // Reverse the linked lists before sending to the Addition class
+                LinkedList reversedShifted = shifted.reverseLinkedList();
+                LinkedList reversedProducts2 = products2.reverseLinkedList();
+
                 LinkedList tempLL = new LinkedList();
-                String products1_as_string = add.add(products1, products2);
-                for (int i = 0; i < products1_as_string.length(); i ++){
-                    tempLL.add_to_list(Character.getNumericValue(products1_as_string.charAt(i)));
+                String products1_as_string = add.add(reversedShifted, reversedProducts2);
+
+                // Reverse the result received from the Addition class
+                String reversedResult = new StringBuilder(products1_as_string).reverse().toString();
+
+                for (int i = 0; i < reversedResult.length(); i++) {
+                    tempLL.add_to_tail(Character.getNumericValue(reversedResult.charAt(i)));
                 }
                 products1 = tempLL;
             }
             current2 = current2.getNext();
-            //partialProduct.clear_list();
-
         }
         StringBuilder result = new StringBuilder();
         Node current = products1.head;
-        while (current != null){
+        while (current != null) {
             result.append(current.getItem());
             current = current.getNext();
         }
-        return result.reverse().toString();
+        return result.toString();
     }
 }
